cd420d3528c15e920d8be2ab72afd8633e6283e3
szoverfi <136331532+Easybisi@users.noreply.github.com>
2024-11-28 13:34:52 +0100

    feladatok

--- task1/task.js
+++ task1/task.js
@@ -1,3 +1,25 @@
 export function findByActorNameAndGenre(allMovies, allActors, actorName, genre) {
 
+
+
+    const actor = allActors.find(actor => actor.name === actorName);
+
+    if (!actor) {
+        return[];
+    }
+   
+    const resmovie = allMovies.filter(movie => {
+        const actorMovie = movie.actor_ids.includes(actor.id);
+
+        const hassGenre = movie.genres.includes(genre);
+
+        return actorMovie && hassGenre;
+
+
+
+        });
+
+        return resmovie.map(movie => movie.title)
+
+
 }
--- task2/task.js
+++ task2/task.js
@@ -1,3 +1,23 @@
 export function findUniqueGenres(genres) {
 
+    let uGenre =[];
+
+    for (let i =0;i < genres.length;i++){
+
+        for (let j = 0;j< genres[i].length;j++){
+
+            const genre = genres[i][j];
+
+            if (!uGenre.includes (genre)){
+                uGenre.push(genre);
+            }
+        }
+      
+    }
+   
+    return uGenre;
+
+
+    
+
 }
